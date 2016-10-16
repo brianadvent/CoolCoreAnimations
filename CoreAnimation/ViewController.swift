44af1878c645542dc891d2226183a12a8d12cd46
Training <training@sinatra.fritz.box>
2016-10-16 14:31:43 +0200

    Cool Core Animation Tutorial

--- CoreAnimation/ViewController.swift
+++ CoreAnimation/ViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var passwordTextField: ShakingTextField!
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        passwordTextField.delegate = self
+        
+        avatarImageView.isUserInteractionEnabled = true
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.addPulse))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        avatarImageView.addGestureRecognizer(tapGestureRecognizer)
+        
+        
+    }
+    
+    
+    func addPulse(){
+        let pulse = Pulsing(numberOfPulses: 1, radius: 110, position: avatarImageView.center)
+        pulse.animationDuration = 0.8
+        pulse.backgroundColor = UIColor.blue.cgColor
+        
+        self.view.layer.insertSublayer(pulse, below: avatarImageView.layer)
+        
+    }
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        passwordTextField.shake()
     }
 
     override func didReceiveMemoryWarning() {
